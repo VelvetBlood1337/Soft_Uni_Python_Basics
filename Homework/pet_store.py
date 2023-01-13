cd7cd596fe16d6849ba29d9eb77e150242904353
VelvetBlood1337 <122019358+VelvetBlood1337@users.noreply.github.com>
2023-01-13 13:07:49 +0200

    Create pet_store.py

---
+++ Homework/pet_store.py
@@ -0,0 +1,4 @@
+number_of_dog_food_packages = int(input())
+number_of_cat_food_packages = int(input())
+total_food_cost = (number_of_dog_food_packages * 2.5) + (number_of_cat_food_packages * 4)
+print(f"{total_food_cost} lv.")
